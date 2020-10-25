a88607f44634123c8c3f337d7c388f85f4b29ea7
Kanis_Fatema_Shanta <33979816+Kanis-Fatema-Shanta@users.noreply.github.com>
2020-10-26 00:03:30 +0600

    set number value

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+//generate pin
+
 document.getElementById("create-pin").addEventListener("click",function(){
     var createPin = Math.round(1000 + Math.random()*9000);
     document.getElementById("pin").value =createPin;
 
 })
+
+// get number
+function number(num){
+    var input = document.getElementById("input-pin");
+    switch(num){
+        case 1:
+            input.value += "1";
+            break;
+        case 2:
+            input.value += "2";
+            break;
+        case 3:
+            input.value += "3";
+            break;
+        case 4:
+            input.value += "4";
+            break;
+        case 5:
+            input.value += "5";
+            break; 
+        case 6:
+            input.value += "6";
+            break; 
+        case 7:
+            input.value += "7";
+            break; 
+        case 8:
+            input.value += "8";
+            break;
+             
+        case 9:
+            input.value += "9";
+            break;
+        case 0:
+            input.value += "0";
+            break;
+    }
+}
+
